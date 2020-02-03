56ed9e785d824f1198aebe9f763d3681c0645c5b
Emil Ingerslev <eki@lunarway.com>
2020-02-03 07:45:15 +0100

    Remove commented code blob

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -91,28 +91,4 @@ func (r *TcpChecker) ReportPortCheck(report tcpchecker.Report) {
 		}
 	}
 	r.previousOpenState = &report.Open
-
-	// switch {
-	// case report.Open && r.lastOpen:
-	// 	// Port is still open - great
-	// case !report.Open && (r.lastOpen || r.lastReport == time.Time{}):
-	// 	// Port closed
-	// 	r.lastReport = time.Now()
-	// 	r.lastOpen = report.Open
-	// 	r.Log.Infof("Port %s:%v status is: %s", report.Address, report.Port, "closed")
-	// case report.Open && !r.lastOpen:
-	// 	// Port opened
-	// 	r.lastReport = time.Now()
-	// 	r.lastOpen = report.Open
-	// 	r.Log.Infof("Port %s:%v status is %s. Content in response: %s", report.Address, report.Port, "open", report.Content)
-	// case !report.Open && !r.lastOpen:
-	// 	// Port still closed
-	// 	if time.Now().Sub(r.lastReport) > 5*time.Minute {
-	// 		r.lastReport = time.Now()
-	// 		r.lastOpen = report.Open
-	// 		r.Log.Infof("Port %s:%v is still %v", report.Address, report.Port, "closed")
-	// 	}
-	// default:
-	// 	panic("This should never happen in LogReporter.ReportPortCheck")
-	// }
 }
